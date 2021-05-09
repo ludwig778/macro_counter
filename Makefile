d9c9079c45fa4ddee6c21197bc75470d448db166
Laurent Arthur <laurent.arthur75@gmail.com>
2021-05-09 23:36:04 +0200

    Add mongo repository with basic tests

--- Makefile
+++ Makefile
@@ -20,6 +20,10 @@ piprot:
 
 sure: lint isort piprot
 
+tests:
+	pytest --cov=macro_counter --cov-append --cov-report html:coverage_html -vs
+.PHONY: tests
+
 clean:
 	rm -rf coverage_html
 	find . -name "*.pyc" -o -name "__pycache__"|xargs rm -rf
---
+++ macro_counter/repository/mongo.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+from macro_counter.settings import MONGO_CONFIG, MONGO_DATABASE, TEST
+
+mongo_client = MongoClient(**MONGO_CONFIG)
+mongo_database = getattr(mongo_client, MONGO_DATABASE)
+
+ingredient_collection = getattr(
+    mongo_database,
+    ("TEST_" if TEST else "") + "ingredient"
+)
