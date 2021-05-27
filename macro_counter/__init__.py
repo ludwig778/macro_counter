a35f1651e3ddd6b86179b0d4118f3334242faa26
Arthur Laurent <laurent.arthur75@gmail.com>
2021-05-27 20:22:11 +0200

    Update how the app is launched

---
+++ macro_counter/__init__.py
@@ -0,0 +1,6 @@
+from macro_counter.prompts.main import MainPrompt
+
+
+def main():
+    prompt = MainPrompt()
+    prompt.loop()
--- macro_counter/__main__.py
+++
@@ -1,5 +0,0 @@
-from macro_counter.prompts.main import MainPrompt
-
-prompt = MainPrompt()
-
-prompt.loop()
