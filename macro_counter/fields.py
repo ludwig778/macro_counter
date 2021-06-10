3416642df9a3ab924112114809bb9959b97717dc
Laurent Arthur <laurent.arthur75@gmail.com>
2021-06-10 02:38:06 +0200

    Add short name to some fields

--- macro_counter/fields.py
+++ macro_counter/fields.py
@@ -1,12 +1,14 @@
 fields = {
     "kcal": {
-        "name": "Calories"
+        "name": "Calories",
+        "shortname": "Cal"
     },
     "units": {
         "name": "Units"
     },
     "protein": {
         "name": "Protein",
+        "shortname": "Prot",
         "macro": True,
         "show_percents": True
     },
@@ -32,21 +34,25 @@ fields = {
     },
     "saturated_fat": {
         "name": "Saturated fat",
+        "shortname": "Sat",
         "macro": False,
         "show_percents": True
     },
     "mono_fat": {
         "name": "Mono sinsaturated fat",
+        "shortname": "Mono",
         "macro": False,
         "show_percents": True
     },
     "poly_fat": {
         "name": "Poly insaturated fat",
+        "shortname": "Poly",
         "macro": False,
         "show_percents": True
     },
     "trans_fat": {
         "name": "Trans fat",
+        "shortname": "Trans",
         "macro": False,
         "show_percents": True
     }
